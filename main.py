1f271983a16a51474e9d1811ab93b7fbee257d10
Omer Belsky <omerbelsky3@gmail.com>
2018-02-10 19:23:36 +0200

    further unspaghettify

--- main.py
+++ main.py
@@ -11,38 +11,19 @@ parser.add_argument('-m', default='k', dest='model', action='store_const', const
 
 knn = KNN.KNN(275)
 
-agemean = 0
-agestd = 0
-maxAge = 0
-maxFare = 0
-faremean = 0
-farestd = 0
-maxSibSp = 0
-maxParch = 0
-minSibSp = sys.maxsize
-minParch = sys.maxsize
-minAge = sys.maxsize
-minFare = float('inf')
-
-testSet = []
-trainSet = []
-trainTargets = []
-
 def createDistrib():
-    global faremean
-    global farestd
-    global agemean
-    global agestd
-    global maxAge
-    global maxFare
-    global maxSibSp
-    global maxParch
-    global minAge
-    global minFare
-    global minSibSp
-    global minParch
     fares = []
     ages = []
+    maxAge = 0
+    maxFare = 0
+    faremean = 0
+    farestd = 0
+    maxSibSp = 0
+    maxParch = 0
+    minSibSp = sys.maxsize
+    minParch = sys.maxsize
+    minAge = sys.maxsize
+    minFare = float('inf')
     with open("./train.csv", 'r')  as reader:
         next(reader)
         for line in reader:
@@ -64,11 +45,12 @@ def createDistrib():
                 maxParch = max(maxParch, parch)
                 minParch = min(minParch, parch)
             except Exception:
-                pass #age was null
-    agemean = np.mean(fares)
-    agestd = np.std(fares)
+                pass
+    faremean = np.mean(fares)
+    farestd = np.std(fares)
     agemean = np.mean(ages)
     agestd = np.std(ages)
+    return faremean, farestd, agemean, agestd, maxAge, minAge, maxFare, minFare, maxSibSp, minSibSp, maxParch, minParch
 
 def predictWithKNN():
     for i in range(len(trainSet)):
@@ -91,50 +73,44 @@ def predictWithSVM():
             writer.write("\n{},{}".format(892 + i, predictions[i]))
 
 def loadData():
-    global testSet
-    global trainSet
+    testSet = []
+    trainSet = []
+    trainTargets = []
     with open("./test.csv", 'r') as reader:
         next(reader)
         for line in reader:
             line = re.split(",(?!\s)", line)
             pClass, gender, age, sibsp, parch, fare = [line[i] for i in [1, 3, 4, 5, 6, 8]]
-            predFare = np.random.normal(faremean, farestd)
-            predAge = int(np.random.normal(agemean, agestd))
-            predFare = min(predFare, minFare)
-            predFare = max(predFare, maxFare)
-            predAge = min(predAge, minAge)
-            predAge = max(predAge, maxAge)
-            features = [(float(pClass) - 1) / 2, 0 if gender == "male" else 1,
-                        int((predAge - minAge) / (maxAge - minAge)) if age == '' else int(
-                            int(float(age) - minAge) / (maxAge - minAge)),
-                        (float(sibsp) - minSibSp) / (maxSibSp - minSibSp),
-                        (float(parch) - minParch) / (maxParch - minParch),
-                        (predFare - minFare) / (maxFare - minFare) if fare == '' else (float(fare) - minFare) / (
-                            maxFare - minFare)]
+            features = createFeatures(pClass, gender, age, sibsp, parch, fare)
             testSet.append(features)
     with open("./train.csv", 'r') as reader:
         next(reader)
         for line in reader:
             line = re.split(",(?!\s)", line)
             survived, pClass, gender, age, sibsp, parch, fare = [line[i] for i in [1, 2, 4, 5, 6, 7, 9]]
-            predFare = np.random.normal(faremean, farestd)
-            predAge = int(np.random.normal(agemean, agestd))
-            predFare = min(predFare, minFare)
-            predFare = max(predFare, maxFare)
-            predAge = min(predAge, minAge)
-            predAge = max(predAge, maxAge)
-            features = [(float(pClass) - 1) / 2, 0 if gender == "male" else 1,
-                        int((predAge - minAge) / (maxAge - minAge)) if age == '' else int(
-                            int(float(age) - minAge) / (maxAge - minAge)),
-                        (float(sibsp) - minSibSp) / (maxSibSp - minSibSp),
-                        (float(parch) - minParch) / (maxParch - minParch),
-                        (predFare - minFare) / (maxFare - minFare) if fare == '' else (float(fare) - minFare) / (
-                        maxFare - minFare)]
+            features = createFeatures(pClass, gender, age, sibsp, parch, fare)
             trainSet.append(features)
             trainTargets.append(survived)
+    return trainSet, trainTargets, testSet
 
-createDistrib()
-loadData()
+def createFeatures(pClass, gender, age, sibsp, parch, fare):
+    predFare = np.random.normal(faremean, farestd)
+    predAge = int(np.random.normal(agemean, agestd))
+    predFare = min(predFare, minFare)
+    predFare = max(predFare, maxFare)
+    predAge = min(predAge, minAge)
+    predAge = max(predAge, maxAge)
+    features = [(float(pClass) - 1) / 2, 0 if gender == "male" else 1,
+                int((predAge - minAge) / (maxAge - minAge)) if age == '' else int(
+                    int(float(age) - minAge) / (maxAge - minAge)),
+                (float(sibsp) - minSibSp) / (maxSibSp - minSibSp),
+                (float(parch) - minParch) / (maxParch - minParch),
+                (predFare - minFare) / (maxFare - minFare) if fare == '' else (float(fare) - minFare) / (
+                    maxFare - minFare)]
+    return features
+
+faremean, farestd, agemean, agestd, maxAge, minAge, maxFare, minFare, maxSibSp, minSibSp, maxParch, minParch = createDistrib()
+trainSet, trainTargets, testSet = loadData()
 if parser.parse_args().model == 'k':
     predictWithKNN()
 else:
